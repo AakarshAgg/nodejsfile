4ea1d50a2878558d601193883922a2504fb5d41b
Aakarsh Aggarwal <110199919+AakarshAgg@users.noreply.github.com>
2023-12-20 21:56:27 +0530

    Update jwtAuth.js

--- middleware/jwtAuth.js
+++ middleware/jwtAuth.js
@@ -16,7 +16,7 @@ const jwtAuth=(req,res,next)=>{
     } catch (error) {
         return res.status(400).json({
             success:false,
-            message:error.message
+            message:"EAuth"
         })
     }
   next()
